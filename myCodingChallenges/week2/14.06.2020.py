7ec72d5a4f2a3a7e03fa9ac5512324df902c3b25
shwet-15 <shwet-15@users.noreply.github.com>
2020-06-14 19:19:01 +0530

    codingPractice

--- myCodingChallenges/week2/14.06.2020.py
+++ myCodingChallenges/week2/14.06.2020.py
@@ -1,3 +1,8 @@
+# a non-empty array of digits representing a non-negative integer, plus one to the integer.
+#The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+#You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+
 def plusOne(digits):
     digits=[str(i) for i in digits]
     digits=''.join(digits)
