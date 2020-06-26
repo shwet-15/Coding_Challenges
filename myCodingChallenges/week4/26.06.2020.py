a8bd61ef99f29a1baf7249cfc6db19244385d98c
shwet-15 <shwet-15@users.noreply.github.com>
2020-06-26 21:03:08 +0530

    codePractice

---
+++ myCodingChallenges/week4/26.06.2020.py
@@ -0,0 +1,12 @@
+def smallerNumbersThanCurrent(nums):
+    l=[]
+    for i in range(len(nums)):
+        c=0
+        for j in range(len(nums)):
+            if(nums[i]>nums[j] and i!=j):
+                 c=c+1
+        l.append(c)
+    return l
+print(smallerNumbersThanCurrent([8,1,2,2,3])) 
+print(smallerNumbersThanCurrent([6,5,4,8])) 
+print(smallerNumbersThanCurrent([7,7,7,7])) 
